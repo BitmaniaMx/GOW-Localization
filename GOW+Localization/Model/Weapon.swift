677966141f70d0ca845c76d3f3b642d42e2f342e
Rafael <rafael.glez@gmail.com>
2024-08-30 21:04:06 -0600

    Contains: Custom font Custom prototipe cells etc

---
+++ GOW+Localization/Model/Weapon.swift
@@ -0,0 +1,8 @@
+//
+//  Weapon.swift
+//  GOW+Localization
+//
+//  Created by Rafael Gonzalez on 30/08/24.
+//
+
+import Foundation
---
+++ GOW+Localization/View/GOWCell.swift
@@ -0,0 +1,23 @@
+//
+//  GOWCell.swift
+//  GOW+Localization
+//
+//  Created by Rafael Gonzalez on 30/08/24.
+//
+
+import UIKit
+
+class GOWCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
